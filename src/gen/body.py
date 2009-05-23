786f9ae1b6c4201c84bfe02cc84bd1d6065699c0
João Rodrigues <joao.marques.rodrigues@gmail.com>
2009-05-23 18:09:43 +0100

    Problemas com indents

--- src/gen/body.py
+++ src/gen/body.py
@@ -16,51 +16,51 @@ def translate_header(f):
 
 def translate_global_list(f):
 	#TODO 
-	#1- Criar um offset para cada variável global
-	#2- Percorrer todas as variáveis globais
-	#3- Consoante o tipo de variável:
-		'''f.write("VAR_TYPE g%d;\n", offset)'''
-	pass 
+	#1- Criar um offset para cada variavel global
+	#2- Percorrer todas as variaveis globais
+	#3- Consoante o tipo de variavel:
+	'''f.write("VAR_TYPE g%d;\n", offset)''' 
+	pass
 		
 def translate_block_list(f):
 	#TODO
-	#1- Para cada bloco de código (frame), verifica se é main ou proc/func
+	#1- Para cada bloco de codigo (frame), verifica se e main ou proc/func
 	#Se main:
-		'''f.write(  "\n/*BLOCO MAIN */\n"
-				"sp=(frame*)malloc(sizeof(frame));\n"
-				"\n/*vardecs*/\n"
-			)
-		declare_local_vars(f)
-		f.write("\n/*statements*/\n")
-		translate_statements(f)'''
+	'''f.write(  "\n/*BLOCO MAIN */\n"
+			"sp=(frame*)malloc(sizeof(frame));\n"
+			"\n/*vardecs*/\n"
+		)
+	declare_local_vars(f)
+	f.write("\n/*statements*/\n")
+	translate_statements(f)'''
 	#Se proc/func:
-		'''pass'''
+	'''pass'''
 	pass
 	
 def declare_local_vars(f):
 	#TODO
-	#1- Percorrer todas as variáveis locais
+	#1- Percorrer todas as variaveis locais
 	#2- Consoante o tipo de variavel:
-		'''f.write("sp->locals[%d]=(VAR_TYPE*)malloc(sizeof(VAR_TYPE));\n")'''
+	'''f.write("sp->locals[%d]=(VAR_TYPE*)malloc(sizeof(VAR_TYPE));\n")'''
 	pass
 
 def translate_statements(f):
 	#TODO
-	#1- Verificar se é do tipo write, assigment ou call 
+	#1- Verificar se e do tipo write, assigment ou call 
 	#Se write:
-		#Verificar se é local ou global
-		#Se global
-			'''f.write(\"%%VAR_TYPE\\n\", g%d);\n", t->offset)'''
-		#Se local
-			'''f.write("printf(\"%%VAR_TYPE\\n\", *((VAR_TYPE*)(sp->locals[%d])));\n", t->offset)'''
+	#Verificar se e local ou global
+	#Se global
+	'''f.write(\"%%VAR_TYPE\\n\", g%d);\n", t->offset)'''
+	#Se local
+	'''f.write("printf(\"%%VAR_TYPE\\n\", *((VAR_TYPE*)(sp->locals[%d])));\n", t->offset)'''
 	#Se assigment:
-		#Verificar se é local ou global
-		#Se global
-			'''f.write("g%d=%VAR_TYPE;\n", t->offset, value)'''
-		#Se local
-			'''f.write("*((VAR_TYPE*)sp->locals[%d])=%VAR_TYPE;\n", t->offset, value)'''
+	#Verificar se e local ou global
+	#Se global
+	'''f.write("g%d=%VAR_TYPE;\n", t->offset, value)'''
+	#Se local
+	'''f.write("*((VAR_TYPE*)sp->locals[%d])=%VAR_TYPE;\n", t->offset, value)'''
 	#Se call: 
-		'''pass'''
+	'''pass'''
 	pass
 
 def translate_footer(f):
