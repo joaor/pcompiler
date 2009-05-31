598e65dd63a50f5d01f07c8935d2ace97cf101eb
João Rodrigues <joao.marques.rodrigues@gmail.com>
2009-05-31 22:39:19 +0100

    Um começo pelo mundo dos procs

--- src/gen/generate.py
+++ src/gen/generate.py
@@ -1,15 +1,22 @@
 f = open("gen/c_code/output.c",'w')
 dic_typ = {"integer":"int","real":"float","char":"char","boolean":"int"}
 dic_type_c = {"int":"%d","float":"%f","char":"%c"}
-dic_trans = {"mod":"%","div":"//"}
+dic_trans = {"mod":"%","div":"/"}
 global_vars = {} #{'y': 'g0', 'a': 'g2', 'c': 'g4', 'z': 'g1', 'b': 'g3'}
 var_type = {} #{'g4': 'integer', 'g3': 'boolean', 'g2': 'char', 'g1': 'real', 'g0': 'real'}
+#WRONG_NUMBER_OF_ARGUMENTS: SCOPEINNER takes exactly 0 arguments (0 given) 
+#VARIABLE_NOT_ASSIGNED: C (INTEGER) has no value
+#Funcoes com o mesmo nome nao da erro
 
 var_counter = 0
+block_flag = False
+MAIN_BLOCK = ""
+ACT_BLOCK = ""
 
 def generate(node):
+	global block_flag,MAIN_BLOCK,ACT_BLOCK
 	if node == None:
-		return None
+		return None	
 
 	elif type(node) == type(""):
 		if node.upper() in ["FALSE","TRUE"]:
@@ -26,9 +33,28 @@ def generate(node):
 			generate(child)
 		translate_footer()
 		f.close()
+	
+	elif node.type in ["block_name"]:
+		b = node.children[0]
+		if not block_flag:
+			MAIN_BLOCK = b
+			block_flag = True
+		ACT_BLOCK = b
+		return ACT_BLOCK
+
+	elif node.type in ["procedure_and_function_declaration_part"]:
+		for child in node.children:
+			generate(child)
+
+	elif node.type in ["procedure_declaration"]:
+		name = generate(node.children[0])
+		translate_proc_1st(name)
+		generate(node.children[0])
+		translate_proc_2nd(name)
 
 	elif node.type in  ["block","variable_declaration_part","variable_declation_list","compound_statement",
-					"statement_sequence","statement","open_statement","closed_statement"]:
+					"statement_sequence","statement","open_statement","closed_statement",
+					"program_heading","proc_or_func_declaration_list","proc_or_func_declaration"]:
 		for child in node.children:
 			generate(child)
 
@@ -70,7 +96,11 @@ def generate(node):
 
 	elif node.type in ["procedure_statement"]:
 		name = generate(node.children[0])
-		params = generate(node.children[1])
+		
+		if len(node.children)!=1:
+			params = generate(node.children[1])
+		else:
+			params = None
 	
 		if name == "writeln" or name == "write":
 			par = params[0][0]
@@ -103,14 +133,16 @@ def generate(node):
 		typ = generate(node.children[1])
 		if len(var) == len(typ):
 			for i in range(len(var)):
-				w = set_var(var[i],typ[i])
-				f.write( "%s %s;\n" % (w[0],w[1]) )
+				if ACT_BLOCK == MAIN_BLOCK:
+					w = set_var(var[i],typ[i])
+					f.write( "%s %s;\n" % (w[0],w[1]) )
 				
 		else:
 			t = typ[0]
 			for v in var:
-				w = set_var(v,t)
-				f.write("%s %s;\n" % (w[0],w[1]))
+				if ACT_BLOCK == MAIN_BLOCK:
+					w = set_var(v,t)
+					f.write("%s %s;\n" % (w[0],w[1]))
 
 def set_var(v,t):
 	global var_counter
@@ -136,8 +168,10 @@ def translate_header():
 			"#define FALSE 0\n"
 			"#define TRUE 1\n\n"
 			"int main()\n{\n"
+			"int _ra;\n"
 			"frame* fp=NULL;\n"
 			"frame* sp=NULL;\n"
+			"sp=(frame*)malloc(sizeof(frame));\n"
 		)
 
 def translate_footer():
@@ -145,7 +179,22 @@ def translate_footer():
 			"}\n\n"
 		)
 
+def translate_proc_1st(name):
+	f.write(  "goto %sskip;\n" % (name) )
+	f.write(  "%s:\n" % (name) )
+	f.write(  "fp=sp;\n"
+			"sp=(frame*)malloc(sizeof(frame));\n"
+			"sp->parent=fp;\n"
+			"sp->return_address=_ra;\n"
+		)
 
+def translate_proc_2nd(name):	
+	f.write(  "_ra=sp->return_address;\n"
+			"sp=sp->parent;\n"
+			"fp=sp->parent;\n"
+			"goto redirector;\n"
+		)
+	f.write(  "%sskip:\n" % (name) )
 
 
 
--- src/tests/test_var.p
+++ src/tests/test_var.p
@@ -4,15 +4,29 @@ y,z : real;
 a : char;
 b : boolean;
 c : integer;
+
+procedure ScopeInner;
+begin
+	writeln(a);
+end;
+
+procedure Sco;
+var Y : integer;
+begin
+	Y := 10;
+	writeln(Y);
+end;
+
 begin 
    z := 2.2;
    y := 1.3 + z;
    a := 'y';
    b := faLse;
-   c := 2 + 5*2 + 4 mod 7;
+   c := 2 + 5*2 + 4 mod 7 div 6;
    write('O mod z e: ');
    writeln(y);
    writeln(a);
    writeln(b);
    write(c);
+
 end.
